f7c04cd5d32d34440ecd144abc44a596c1be5a70
Aleksey <sidorenkoalekse@mail.ru>
2022-10-19 18:09:55 +0700

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,44 +1,19 @@
-const sliderMain = new Swiper('.slider__main', {
-    freeMode: true,
-    centeredSlides: true,
-    mousewheel: true,
-    parallax: true,
-    breakpoints: {
-        0: {
-            slidesPerView: 1.5,
-            spaceBetween: 20
-        },
-        680: {
-            slidesPerView: 2,
-            spaceBetween: 40
-        },
-        1400: {
-            slidesPerView: 3.5,
-            spaceBetween: 60
-        }
-    }
-})
-
-const sliderBg= new Swiper('.slider__bg', {
-    centeredSlides: true,
-    parallax: true,
-    spaceBetween: 60,
-    slidesPerView: 3.5
-})
-
-sliderMain.controller.control = sliderBg;
-
-document.querySelectorAll('.slider__item').forEach(item => {
-    item.addEventListener('click', event => {
-        /* Добавление класса при клике и удаление его при втором клике */
-        item.classList.toggle('opened');
+window.onload = function() {
+    
+    setTimeout(function() {
+    
+    //Добавление body класса loaded после 200ms
+    document.body.classList.add('loaded');
+        
+    //Draggable - библиотека для свободного перемещения в div
+    //Применяется для div gallery
+    //bounds - параметр для "отскока" от края div
+    //inertia применяется совместно с библиотекой InertiaPlugin для плавного "инерционного" перемещения
+    Draggable.create('.gallery', {
+        bounds: 'body',
+        inertia: true
     });
-});
-
-
-let desc = document.querySelector('.description');
-
-sliderMain.on('slideChange', e => {
-    /* Упрощенная форма записи if - else */
-    sliderMain.activeIndex > 0 ? desc.classList.add("hidden") : desc.classList.remove("hidden");
-});
+        
+    }, 200)
+    
+}
