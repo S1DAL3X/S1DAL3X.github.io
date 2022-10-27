ab61a717621fa2539f882f0c770676bdac4009a8
Aleksey <sidorenkoalekse@mail.ru>
2022-10-27 21:53:53 +0700

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,15 +1,14 @@
+AOS.init();
+
 var menuList = document.querySelector('.header__menu');
-// var searchIcon = document.querySelector('.search');
 var burger = document.querySelector('.burger');
 
 function navToggle(){
     if(menuList.classList.contains('show')){
         menuList.classList.remove('show');
-        // searchIcon.classList.remove('hide');
         burger.classList.remove ('close');
     } else{
         menuList.classList.add('show');
-        // searchIcon.classList.add('hide');
         burger.classList.add('close');
     }
 }
