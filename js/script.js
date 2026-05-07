c173b064c3d85a533aff12d2e4e7ff61ce7ae9a9
Aleksey <sidorenkoalekse@mail.ru>
2026-05-07 22:16:03 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,14 +4,21 @@ $(document).ready(function() {
     const preloader = document.querySelector('.preloader');
     const preloader_img = document.querySelector('.preloader_img');
     const preloader_button = document.querySelector('.preloader_button');
+    const audio = document.querySelector('.audio');
 
     
     main.style.display = "none";
+    audio.style.display = "none";
     $(".preloader_button").on( "click", function() {
         main.style.display = "block";
         setTimeout(function() {
             preloader.style.display = "none";
-        }, 1500);
+        }, 2000);
     });
-    
+    $(".preloader_button").on( "click", function() {
+        main.style.display = "block";
+        setTimeout(function() {
+            audio.style.display = "block";
+        }, 500);
+    });
 });
